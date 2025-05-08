148f539f3b238cabd47fc7d63f0d876dadbbb6ba
Yan P <y.pilon9@gmail.com>
2025-05-08 16:44:49 -0400

    load boot and cartridge roms

--- main.cpp
+++ main.cpp
@@ -1,28 +1,87 @@
 #include <SDL.h>
 #include <temp.h>
 
-int main(int argc, char* argv[])
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <cstdint>
+#include <thread>
+#include <chrono>
+
+/*
+    Util function that loads a file into a unsigned int8 vector
+*/
+enum Status {
+    OK,
+    ERR_OPEN,
+    ERR_OVERFLOW
+};
+
+Status LoadFileToBuffer(const std::string& filePath, std::vector<std::uint8_t>& buffer) {
+    std::ifstream file(filePath, std::ios::binary);
+    if (!file) {
+        return Status::ERR_OPEN;
+    }
+
+    std::uint8_t byte;
+    size_t r_index{0};
+
+    while (file.read(reinterpret_cast<char*>(&byte), 1)) {
+        if (r_index >= buffer.size()) {
+            return Status::ERR_OVERFLOW;
+        }
+        buffer[r_index++] = byte;
+    }
+
+    return Status::OK;
+}
+
+int main(int argc, char *argv[])
 {
-    SDL_Init(SDL_INIT_VIDEO);
+    if (argc < 2) {
+        std::cout << "No file path provided";
+        return 1;
+    }
+    const std::string cartridge_filepath = argv[1];
 
-    SDL_Window* window = SDL_CreateWindow(
-        "SDL2Test",
-        SDL_WINDOWPOS_UNDEFINED,
-        SDL_WINDOWPOS_UNDEFINED,
-        640,
-        480,
-        0
-    );
+    //initialize ram and load program
+    int kb{ 1024 };
+    int cart_rom_size{ 32*kb }; //TODO: Read cartridge rom header and allocate appropriate amount of memory for ROM
+    std::vector<std::uint8_t> cart_rom(cart_rom_size,0);
 
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-    SDL_RenderClear(renderer);
-    SDL_RenderPresent(renderer);
+    switch (LoadFileToBuffer(cartridge_filepath, cart_rom))
+    {
+    case ERR_OPEN:
+        std::cerr << "Error: Failed to open the cartridge rom file." << cartridge_filepath << std::endl;
+        return 1;
+        break;
+    case ERR_OVERFLOW:
+        std::cerr << "Error: cartridge rom file is too big for memory." << std::endl;
+        return 1;
+        break;
+    default:
+        break;
+    }
 
-    SDL_Delay(3000);
+    //Load Boot rom
+    std::vector<std::uint8_t> boot_rom(256,0);
+    switch (LoadFileToBuffer("C:/Users/Yan/Desktop/gameboy/dmg_boot.bin", boot_rom)) //TODO: Change to use a relative path
+    {
+    case ERR_OPEN:
+        std::cerr << "Error: Could not load the boot rom, make sure it is in the same directory as the executable." << std::endl;
+        return 1;
+        break;
+    case ERR_OVERFLOW:
+        std::cerr << "Error: boot rom file is too big for memory." << std::endl;
+        return 1;
+        break;
+    default:
+        break;
+    }
 
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-
+    //run program
+    std::this_thread::sleep_for(std::chrono::milliseconds(3000));
     return 0;
 }
+
