d87669a29d7aca20f33c202eed52814159d1757a
Yan P <y.pilon9@gmail.com>
2025-11-09 22:43:07 -0500

    refactor bus

--- src/GBBus.cpp
+++ src/GBBus.cpp
@@ -1,80 +1,38 @@
 #include <GBBus.h>
 
-GBBus::GBBus(std::vector<uint8_t>& boot_ROM, std::vector<uint8_t>& cartridge_ROM):
-    boot_ROM{boot_ROM},
-    cartridge_ROM{cartridge_ROM},
-    v_ram(8192),
-    ext_ram(8192),
-    w_ram(8192),
-    echo_ram(7680),
-    obj_attribute_mem(160),
-    io_registers(128),
-    h_ram(127),
-    ie_register{0}
+GBBus::GBBus(std::vector<uint8_t>& bootROM, std::vector<uint8_t>& cartridgeROM):
+    bootROM{bootROM},
+    cartridgeROM{cartridgeROM}
 {
+    this->memory = vector<uint8_t>(MEMORY_SIZE, 0);
 }
 
+//TODO: return 0xFF on OAM blocked reads during DMA transfer
 uint8_t GBBus::read(uint16_t address){
     if(address <= 0x7FFF){
-        if(address <= 0x00FF){
-            if(io_registers[0x50] == 0){ //1 Boot rom is disabled, read from cartridge, 0 boot rom is active
-                return boot_ROM[address];
-            }else{
-                return cartridge_ROM[address];
-            }
+        if(address <= 0x00FF && memory[BOOTLOCK_ADDR] == BOOTLOCK_ACTIVE){
+            return bootROM[address];
         }else{
-            return cartridge_ROM[address];
+            return cartridgeROM[address];
         }
-    } else if(0x8000 <= address && address <= 0x9FFF){
-        return v_ram[address % 0x8000];
-    } else if(0xA000 <= address && address <= 0xBFFF){
-        return ext_ram[address % 0xA000];
-    } else if(0xC000 <= address && address <= 0xDFFF){
-        return w_ram[address % 0xC000];
-    } else if(0xE000 <= address && address <= 0xFDFF){
-        return echo_ram[address % 0xE000];
-    } else if(0xFE00 <= address && address <= 0xFE9F){
-        return obj_attribute_mem[address % 0xFE00];
-    } else if (0xFF00 <= address && address <= 0xFF7F){ //h_ram access
-        return io_registers[address % 0xFF00];
-    } else if (0xFF80 <= address && address <= 0xFFFE){ //h_ram access
-        return h_ram[address % 0xFF80];
-    } else if (address == 0xFFFF){
-        return ie_register;
-    } else {
-        return 0xFF;
+    } else if (address <= MEMORY_SIZE - 1){
+        return memory[address];
+    } else { // Invalid address
+        return 0xFF; 
     }
 }
 
-//TODO: route reads to cartridge rom to MBC registers
 void GBBus::write(uint8_t value, uint16_t address){
     if(address <= 0x7FFF){
-        if(address <= 0x00FF){
-            if(io_registers[0x50] == 0){ //1 Boot rom is disabled, read from cartridge, 0 boot rom is active
-                //ignore writes to boot ROM
-            }else{
-                cartridge_ROM[address] = value;
-            }
-        }else{
-            cartridge_ROM[address] = value;
-        }
-    } else if(0x8000 <= address && address <= 0x9FFF){
-        v_ram[address % 0x8000] = value;
-    } else if(0xA000 <= address && address <= 0xBFFF){
-        ext_ram[address % 0xA000] = value;
+        if(address <= 0x00FF && memory[BOOTLOCK_ADDR] == BOOTLOCK_ACTIVE) return; //Ignore writes to boot ROM
+        cartridgeROM[address] = value;
     } else if(0xC000 <= address && address <= 0xDFFF){
-        w_ram[address % 0xC000] = value;
-        if(address <= 0xDDFF) echo_ram[address % 0xC000] = value; //emulate mirroring between w_ram and echo_ram
+        memory[address] = value; // work ram
+        if(address <= 0xDDFF) memory[address + 0x2000] = value; // echo ram
     } else if(0xE000 <= address && address <= 0xFDFF){
-        echo_ram[address % 0xE000] = value;
-        w_ram[address % 0xE000] = value; //emulate mirroring between w_ram and echo_ram
-    } else if(0xFE00 <= address && address <= 0xFE9F){
-        obj_attribute_mem[address % 0xFE00] = value;
-    } else if (0xFF00 <= address && address <= 0xFF7F){ //h_ram access
-        io_registers[address % 0xFF00] = value;
-    } else if (0xFF80 <= address && address <= 0xFFFE){ //h_ram access
-        h_ram[address % 0xFF80] = value;
-    } else if (address == 0xFFFF){
-        ie_register = value;
+        memory[address] = value; // echo ram
+        memory[address - 0x2000] = value; // work ram
+    } else if (address <= MEMORY_SIZE - 1) {
+        memory[address] = value;
     }
 }
--- src/GBBus.h
+++ src/GBBus.h
@@ -1,46 +1,65 @@
 #pragma once
 #include <vector>
+using std::vector;
 
-#define IE_ADDR 0xFFFF
-#define IF_ADDR 0xFF0F
-#define BOOTLOCK_ADDR 0xFF50
-#define DIV_ADDR 0xFF04
-#define TIMA_ADDR 0xFF05
-#define TMA_ADDR 0xFF06
-#define TAC_ADDR 0xFF07
-#define JOYP_ADDR 0xFF00
+constexpr const uint32_t MEMORY_SIZE = 0x10000; //64KB
+
+constexpr const uint16_t BOOTLOCK_DISABLED = 1;
+constexpr const uint16_t BOOTLOCK_ACTIVE = 0;
+
+constexpr const uint16_t IE_ADDR = 0xFFFF;
+constexpr const uint16_t IF_ADDR = 0xFF0F;
+constexpr const uint16_t BOOTLOCK_ADDR = 0xFF50;
+constexpr const uint16_t DIV_ADDR = 0xFF04;
+constexpr const uint16_t TIMA_ADDR = 0xFF05;
+constexpr const uint16_t TMA_ADDR = 0xFF06;
+constexpr const uint16_t TAC_ADDR = 0xFF07;
+constexpr const uint16_t JOYP_ADDR = 0xFF00;
 
 //graphics
-#define LCDC_ADDR 0xFF40
-#define STAT_ADDR 0xFF41
-#define SCY_ADDR 0xFF42
-#define SCX_ADDR 0xFF43
-#define LY_ADDR 0xFF44
-#define LYC_ADDR 0xFF45
-#define WY_ADDR 0xFF4A
-#define WX_ADDR 0xFF4B
-#define DMA_ADDR 0xFF46 //writing to this starts obj_attribute_mem DMA, lasts 160 M-cycles, bus is blocked for cpu, ppu cant read obj_attribute_mem
-#define BGP_ADDR 0xFF47
-#define OBP0_ADDR 0xFF48
-#define OBP1_ADDR 0xFF49
+constexpr const uint16_t LCDC_ADDR = 0xFF40;
+constexpr const uint16_t STAT_ADDR = 0xFF41;
+constexpr const uint16_t SCY_ADDR = 0xFF42;
+constexpr const uint16_t SCX_ADDR = 0xFF43;
+constexpr const uint16_t LY_ADDR = 0xFF44;
+constexpr const uint16_t LYC_ADDR = 0xFF45;
+constexpr const uint16_t WY_ADDR = 0xFF4A;
+constexpr const uint16_t WX_ADDR = 0xFF4B;
+
+// writing to this starts obj_attribute_mem DMA, lasts 160 M-cycles, bus is blocked for cpu, ppu cant read obj_attribute_mem
+constexpr const uint16_t DMA_ADDR = 0xFF46; 
+
+constexpr const uint16_t BGP_ADDR = 0xFF47;
+constexpr const uint16_t OBP0_ADDR = 0xFF48;
+constexpr const uint16_t OBP1_ADDR = 0xFF49;
+
+/*
+Mappings:
+0000 - 00FF   Boot ROM (mapped only if BOOTLOCK is active)
+0000 - 7FFF   Cartridge ROM
+8000 - 9FFF   Video RAM (VRAM)
+A000 - BFFF   External RAM (cartridge RAM)
+C000 - DFFF   Work RAM (WRAM)
+E000 - FDFF   Echo RAM (mirrors C000 - DDFF)
+FE00 - FE9F   Sprite Attribute Table (OAM)
+FEA0 - FEFF   Not Usable
+FF00 - FF7F   I/O Registers
+FF80 - FFFE   High RAM (HRAM)
+FFFF         Interrupt Enable Register (IE)
+*/
 
 class GBBus{
     private:
-        std::vector<uint8_t> boot_ROM; //0000 - 00FF
-        std::vector<uint8_t> cartridge_ROM; //0000 - 3FFF fixed, 4000 - 7FFF switchable. Will probably need to create cartridge class to hide mapper from bus
-
-        std::vector<uint8_t> v_ram; //8000 - 9FFF
-        std::vector<uint8_t> ext_ram; //A000 - BFFF
-        std::vector<uint8_t> w_ram; //C000 - DFFF
-        std::vector<uint8_t> echo_ram; //E000 - FDFF
-        std::vector<uint8_t> obj_attribute_mem; //FE00 - FE9F
-        std::vector<uint8_t> h_ram; //FE00 - FE9F
-        std::vector<uint8_t> io_registers; //FF00 - FF7F
-
-        uint8_t ie_register; //FFFF
+        /*
+        0000 - 3FFF fixed, 4000 - 7FFF switchable
+        TODO: create cartridge class to hide mapper from bus
+        */
+        vector<uint8_t> cartridgeROM; 
+        vector<uint8_t> bootROM;
+        vector<uint8_t> memory;
 
     public:
-        GBBus(std::vector<uint8_t>& boot_ROM, std::vector<uint8_t>& cartridge_ROM);
+        GBBus(vector<uint8_t>& bootROM, vector<uint8_t>& cartridgeROM);
         uint8_t read(uint16_t address);
         void write(uint8_t value, uint16_t address);
 };
